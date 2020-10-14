606eca8a608079bf06fcc70006662e53aa18e971
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-14 09:45:06 +0530

    6digits program has added and not accept alphabet and end

--- pincodeValidtion.sh
+++ pincodeValidtion.sh
@@ -4,9 +4,10 @@ echo "welcome to pincodevalidation progarm"
 
 ##program to accept 6 digits
 ##not to accept alphabet
+##not to accept alphabets at end
 read -p "enter pincode: " pincode
 
-pattern='^[0-9]{6}'
+pattern='^[0-9]{6}$'
 
 if [[ $pincode =~ $pattern ]]
 then
