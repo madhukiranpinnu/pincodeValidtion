7f83e8ab8dd2f41b999929a8f7a5b8649e50650b
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-14 09:49:16 +0530

    6digits program has added and not accept alphabet and end and spces in between

--- pincodeValidtion.sh
+++ pincodeValidtion.sh
@@ -5,10 +5,10 @@ echo "welcome to pincodevalidation progarm"
 ##program to accept 6 digits
 ##not to accept alphabet
 ##not to accept alphabets at end
+#even accept with spaces
 read -p "enter pincode: " pincode
 
-pattern='^[0-9]{6}$'
-
+pattern='^[0-9]{3}[[:space:]]{0,1}[0-9]{3}$'
 if [[ $pincode =~ $pattern ]]
 then
         echo "valid"
