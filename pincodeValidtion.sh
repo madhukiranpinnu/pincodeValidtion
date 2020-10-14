05a41fa2a2ca05b92c21d870ea0dc8b533dd346b
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-14 09:37:17 +0530

    6digits program has added

--- pincodeValidtion.sh
+++ pincodeValidtion.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "welcome to pincodevalidation progarm"
+
+##program to accept 6 digits
+read -p "enter pincode: " pincode
+
+pattern='[0-9]{6}'
+
+if [[ $pincode =~ $pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
